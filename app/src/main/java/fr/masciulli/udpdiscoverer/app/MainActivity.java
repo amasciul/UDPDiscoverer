f08a061568b6d8b5202b94bf22097fc37527b585
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2015-04-04 16:33:07 +0200

    Replace button by action

--- app/src/main/java/fr/masciulli/udpdiscoverer/app/MainActivity.java
+++ app/src/main/java/fr/masciulli/udpdiscoverer/app/MainActivity.java
@@ -3,7 +3,9 @@ package fr.masciulli.udpdiscoverer.app;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -14,7 +16,6 @@ import fr.masciulli.udpdiscoverer.lib.Discoverer;
 public class MainActivity extends ActionBarActivity implements Callback {
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private Button sendButton;
     private EditText messageField;
     private EditText localPortField;
     private EditText remotePortField;
@@ -24,24 +25,9 @@ public class MainActivity extends ActionBarActivity implements Callback {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        sendButton = (Button) findViewById(R.id.send);
         messageField = (EditText) findViewById(R.id.message);
         localPortField = (EditText) findViewById(R.id.local_port);
         remotePortField = (EditText) findViewById(R.id.remote_port);
-
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String message = messageField.getText().toString();
-                try {
-                    int localPort = Integer.parseInt(localPortField.getText().toString());
-                    int remotePort = Integer.parseInt(remotePortField.getText().toString());
-                    sendMessage(message, localPort, remotePort);
-                } catch (NumberFormatException exception) {
-                    Toast.makeText(MainActivity.this, getString(R.string.port_format_error), Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
     }
 
     private void sendMessage(String message, int localPort, int remotePort) {
@@ -51,6 +37,29 @@ public class MainActivity extends ActionBarActivity implements Callback {
                 .data(message.getBytes())
                 .callback(this)
                 .broadcast();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_send) {
+            String message = messageField.getText().toString();
+            try {
+                int localPort = Integer.parseInt(localPortField.getText().toString());
+                int remotePort = Integer.parseInt(remotePortField.getText().toString());
+                sendMessage(message, localPort, remotePort);
+            } catch (NumberFormatException exception) {
+                Toast.makeText(MainActivity.this, getString(R.string.port_format_error), Toast.LENGTH_SHORT).show();
+            }
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
